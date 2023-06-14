ad0bd5f57fa91f24f64e78bc5a8fe95ed13a744d
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-13 20:43:50 -0400

    added tests for two_sum.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from two_sum import twoSum
+
+if __name__ == '__main__':
+    assert twoSum(nums = [2,7,11,15], target = 9) == [0,1]
+    assert twoSum(nums = [3,3], target = 6) == [0, 1]
+    assert twoSum(nums = [3,2,4], target = 6) == [1, 2]
+    print("Passed all the tests for two_sum 🎉")
