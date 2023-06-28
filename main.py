702b61f3d583ccee310eb0d6c036b10cf0a33248
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-27 21:59:24 -0400

    added tests for is_valid

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from week1.two_sum import two_sum
+from week1.is_valid import is_valid
 
 if __name__ == '__main__':
     assert two_sum(nums = [2,7,11,15], target = 9) == [0,1]
     assert two_sum(nums = [3,3], target = 6) == [0, 1]
     assert two_sum(nums = [3,2,4], target = 6) == [1, 2]
     print("Passed all the tests for two_sum 🎉")
+
+    assert is_valid(s = "()") == True
+    assert is_valid(s = "()[]{}") == True
+    assert is_valid(s = "(]") == False
+    print("Passed all the tests for is_valid 🎉")
