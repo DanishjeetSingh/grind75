e869dbcfb2e470148ef1dd0a055add0d3cb3a90d
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-27 21:55:50 -0400

    added comments

--- week1/is_valid.py
+++ week1/is_valid.py
@@ -5,8 +5,10 @@ def is_valid(self, s: str) -> bool:
         if char in ['(', '{', '[']:
             stack.append(char)
         else:
+            # if stack is empty then the order of parentheses is wrong, return false
             if not stack:
                 return False
+            # if the match is right then keep popping from the stack and keep moving on
             elif char == ')' and stack[-1] == '(':
                 stack.pop()
             elif char == ']' and stack[-1] == '[':
@@ -14,6 +16,8 @@ def is_valid(self, s: str) -> bool:
             elif char == '}' and stack[-1] == '{':
                 stack.pop()
             else:
+                # if you have added a open parentheses to the stack, but the right closing paren isn't found, return false
                 return False
 
+    # at the end if the stack is empty then return true, else False
     return not stack
