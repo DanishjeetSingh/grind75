5082762ced23be555ece937b5bfbca4e0d9ed157
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-27 21:51:56 -0400

    fixed imports and funciton calling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from week1.two_sum import twoSum
+from week1.two_sum import two_sum
 
 if __name__ == '__main__':
-    assert twoSum(nums = [2,7,11,15], target = 9) == [0,1]
-    assert twoSum(nums = [3,3], target = 6) == [0, 1]
-    assert twoSum(nums = [3,2,4], target = 6) == [1, 2]
+    assert two_sum(nums = [2,7,11,15], target = 9) == [0,1]
+    assert two_sum(nums = [3,3], target = 6) == [0, 1]
+    assert two_sum(nums = [3,2,4], target = 6) == [1, 2]
     print("Passed all the tests for two_sum 🎉")
