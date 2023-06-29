76d882467cda7252db541249908ba1e64aa8b664
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-28 22:49:35 -0400

    added more tests and imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from week1 import two_sum, is_valid
+from week1 import two_sum, is_valid, merge_two_lists, ListNode, ListNode_to_list
 
 if __name__ == '__main__':
     assert two_sum(nums = [2,7,11,15], target = 9) == [0,1]
@@ -10,3 +10,9 @@ if __name__ == '__main__':
     assert is_valid(s = "()[]{}") == True
     assert is_valid(s = "(]") == False
     print("Passed all the tests for is_valid 🎉")
+
+    assert ListNode_to_list(merge_two_lists(None, None)) == []
+    assert ListNode_to_list(merge_two_lists(ListNode(1, ListNode(2, ListNode(4))),
+                                            ListNode(1, ListNode(3, ListNode(4))))) == [1, 1, 2, 3, 4, 4]
+    assert ListNode_to_list(merge_two_lists(None, ListNode())) == [0]
+    print("Passed all the tests for merge_two_lists 🎉")
